b97b151e5fd95f445a0ac62f527211d1b10685b9
restumahesa26 <mufti.restumahesa@gmail.com>
2021-03-28 12:26:07 +0700

    upload tugas 1

---
+++ Functional Programming.py
@@ -0,0 +1,12 @@
+def shout(text): 
+  return text.upper() 
+    
+def whisper(text): 
+  return text.lower() 
+    
+def greet(func): 
+  greeting = func("Hi, I am created by a function passed as an argument.") 
+  print(greeting)  
+    
+greet(shout) 
+greet(whisper) 
